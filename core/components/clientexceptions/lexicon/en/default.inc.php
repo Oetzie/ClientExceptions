77670812000b6f98e9dafddd821e256ef5f7ec95
Rinze van der Brug <rinze@sterc.nl>
2017-06-02 08:48:39 +0200

    Improved Dutch/English lexicons, add README.md, expanded readme.txt

--- core/components/clientexceptions/lexicon/en/default.inc.php
+++ core/components/clientexceptions/lexicon/en/default.inc.php
@@ -19,56 +19,55 @@
 	 * Suite 330, Boston, MA 02111-1307 USA
 	 */
 
-	$_lang['clientexceptions'] 											= 'IP exception';
-	$_lang['clientexceptions.desc'] 									= 'Change or create IP exceptions.';
+	$_lang['clientexceptions'] 											= 'IP Exception';
+	$_lang['clientexceptions.desc'] 									= 'Manage your IP exceptions.';
 	
-	$_lang['area_clientexceptions'] 									= 'IP exception';
+	$_lang['area_clientexceptions'] 									= 'IP Exception';
 			
 	$_lang['clientexceptions.exception']								= 'Exception';
 	$_lang['clientexceptions.exceptions']								= 'Exceptions';
-	$_lang['clientexceptions.exceptions_desc']							= 'Here you can manage the IP exceptions for your site. An IP exception is meant to give some one with use of his or her IP number grant or deny to the website.';
+	$_lang['clientexceptions.exceptions_desc']							= 'Manage the IP exceptions for your website here! This Extra allows you to control which IP addresses do or do not have permission to visit your MODX website.';
 	$_lang['clientexceptions.exception_create']							= 'Create new exception';
-	$_lang['clientexceptions.exception_update']							= 'Update exception.';
+	$_lang['clientexceptions.exception_update']							= 'Update exception';
 	$_lang['clientexceptions.exception_remove']							= 'Delete exception';
 	$_lang['clientexceptions.exception_remove_confirm']					= 'Are you sure you want to delete this exception?';
 	$_lang['clientexceptions.exceptions_import']						= 'Import exceptions';
-	$_lang['clientexceptions.exceptions_import_desc']					= 'Select a CSV file to import the exceptions. It must be a valid CSV format.';
+	$_lang['clientexceptions.exceptions_import_desc']					= 'Select a CSV file to import the exceptions. Make sure it is a valid CSV format.';
 	$_lang['clientexceptions.exceptions_export']						= 'Export exceptions';
 	
-	$_lang['clientexceptions.label_ipnumber']							= 'IP number';
-	$_lang['clientexceptions.label_ipnumber_desc']						= 'The IP number of the exception. For a wildcard in the IP number use %, use ^ to start an IP range (ex. ^127.0.) or use $ to end an IP range (ex. .0.1$).';
+	$_lang['clientexceptions.label_ipnumber']							= 'IP address';
+	$_lang['clientexceptions.label_ipnumber_desc']						= 'Use ^ to start an IP range (ex. ^127.0.), use $ to end an IP range (ex. .0.1$) and use % to activate a wildcard.';
 	$_lang['clientexceptions.label_own_ipnumber']						= 'My IP';
-	$_lang['clientexceptions.label_name']								= 'Name';
-	$_lang['clientexceptions.label_name_desc']							= 'The name of the exception.';
+	$_lang['clientexceptions.label_name']								= 'Name exception';
+	$_lang['clientexceptions.label_name_desc']							= 'Enter the name of the exception.';
 	$_lang['clientexceptions.label_context']							= 'Context';
-	$_lang['clientexceptions.label_context_desc']						= 'The context of the exception. If there is no context selected the exception will be valid on all contexts.';
-	$_lang['clientexceptions.label_type']								= 'Exceptions type';
-	$_lang['clientexceptions.label_type_desc']							= 'The type of the exception, this can be "grant" or "deny".';
-	$_lang['clientexceptions.label_description']						= 'Description/reason';
-	$_lang['clientexceptions.label_description_desc']					= 'The description/reason of the exception.';
+	$_lang['clientexceptions.label_context_desc']						= 'Select the context for which the exception should be active. If no context is selected, the exception will be active in all contexts!';
+	$_lang['clientexceptions.label_type']								= 'Exception type';
+	$_lang['clientexceptions.label_type_desc']							= 'Grant or deny access of this exception.';
+	$_lang['clientexceptions.label_description']						= 'Description (optional)';
+	$_lang['clientexceptions.label_description_desc']					= 'Add a description.';
 	$_lang['clientexceptions.label_active']								= 'Active';
 	$_lang['clientexceptions.label_active_desc']						= '';
 	$_lang['clientexceptions.label_import_file']						= 'File';
 	$_lang['clientexceptions.label_import_file_desc']					= 'Select a valid CSV file.';
 	$_lang['clientexceptions.label_import_delimiter']					= 'Delimiter';
-	$_lang['clientexceptions.label_import_delimiter_desc']				= 'The delimiter to separate the columns. Default is ";".';
-	$_lang['clientexceptions.label_import_headers']						= 'First rows are columns';
+	$_lang['clientexceptions.label_import_delimiter_desc']				= 'The delimiter to separate columns. Default is ";".';
+	$_lang['clientexceptions.label_import_headers']						= 'First row consists of columns.';
 	$_lang['clientexceptions.label_import_headers_desc']				= '';
-	$_lang['clientexceptions.label_import_reset']						= 'Delete all current exeptions.';
+	$_lang['clientexceptions.label_import_reset']						= 'Delete all current exceptions.';
 	$_lang['clientexceptions.label_import_reset_desc']					= '';
 
 	$_lang['clientexceptions.filter_context']							= 'Filter on context...';
 	$_lang['clientexceptions.filter_type']								= 'Filter on type...';
 	$_lang['clientexceptions.context_independent']						= 'Independent';	
-	$_lang['clientexceptions.type_deny']								= 'Deny';
-	$_lang['clientexceptions.type_grant']								= 'Grant';
+	$_lang['clientexceptions.type_deny']								= 'Deny access';
+	$_lang['clientexceptions.type_grant']								= 'Grant access';
 	$_lang['clientexceptions.import_dir_failed']						= 'An error occurred while importing the exceptions, the import directory could not be created.';
-	$_lang['clientexceptions.import_valid_failed']						= 'Select a valid CSV file..';
+	$_lang['clientexceptions.import_valid_failed']						= 'Select a valid CSV file!';
 	$_lang['clientexceptions.import_upload_failed']						= 'An error occurred while importing the exceptions, the CSV could not be uploaded.';
 	$_lang['clientexceptions.import_read_failed']						= 'An error occurred while importing the exceptions, the CSV could not be not be read.';
-	$_lang['clientexceptions.export_failed']							= 'An error occurred while exporting the exceptions, try again.';
-	$_lang['clientexceptions.export_dir_failed']						= 'An error occurred while exporting the exceptions, the import directory could not be created.';
+	$_lang['clientexceptions.export_failed']							= 'An error occurred while exporting the exceptions. Try again, please.';
+	$_lang['clientexceptions.export_dir_failed']						= 'An error occurred while exporting the exceptions, the export directory could not be created.';
 	
-	$_lang['clientexceptions.error_own_ip']								= 'It is not possible to deny your own acces.';
-	
+	$_lang['clientexceptions.error_own_ip']								= 'It is not possible to deny your own access!';
 ?>
--- core/components/clientexceptions/lexicon/nl/default.inc.php
+++ core/components/clientexceptions/lexicon/nl/default.inc.php
@@ -19,40 +19,40 @@
 	 * Suite 330, Boston, MA 02111-1307 USA
 	 */
 
-	$_lang['clientexceptions'] 											= 'IP uitzonderingen';
-	$_lang['clientexceptions.desc'] 									= 'Wijzig of maak IP uitzonderingen.';
+	$_lang['clientexceptions'] 											= 'IP Exception';
+	$_lang['clientexceptions.desc'] 									= 'Beheer je IP uitzonderingen!';
 	
-	$_lang['area_clientexceptions']										= 'IP uitzonderingen';
+	$_lang['area_clientexceptions']										= 'IP Exception';
 
 	$_lang['clientexceptions.exception']								= 'Uitzondering';
 	$_lang['clientexceptions.exceptions']								= 'Uitzonderingen';
-	$_lang['clientexceptions.exceptions_desc']							= 'Hier kun je alle IP uitzonderingen beheren voor jouw website. Een IP uitzondering is bedoelt om iemand aan de hand van zijn of haar IP nummer toegang tot de website te verlenen of weigeren.';
+	$_lang['clientexceptions.exceptions_desc']							= 'Beheer de IP uitzonderingen van jouw website hier! Deze Extra stelt jou in staat om te beheren welke IP adressen wel of geen toegang krijgen om jouw website te bezoeken.';
 	$_lang['clientexceptions.exception_create']							= 'Nieuwe uitzondering';
 	$_lang['clientexceptions.exception_update']							= 'Uitzondering wijzigen';
 	$_lang['clientexceptions.exception_remove']							= 'Uitzondering verwijderen';
 	$_lang['clientexceptions.exception_remove_confirm']					= 'Weet je zeker dat je deze uitzondering wilt verwijderen?';
 	$_lang['clientexceptions.exceptions_import']						= 'Uitzonderingen importeren';
-	$_lang['clientexceptions.exceptions_import_desc']					= 'Selecteer een CSV bestand om uitzonderingen te importeren. Het moet een geldig CSV formaat zijn.';
+	$_lang['clientexceptions.exceptions_import_desc']					= 'Selecteer een CSV bestand om uitzonderingen te importeren. Dit moet een geldig CSV formaat zijn.';
 	$_lang['clientexceptions.exceptions_export']						= 'Uitzonderingen exporteren';
 	
-	$_lang['clientexceptions.label_ipnumber']							= 'IP nummer';
-	$_lang['clientexceptions.label_ipnumber_desc']						= 'Het IP nummer van de uitzondering. Voor een wildcard in het IP nummer gebruik %, gebruik een ^ om een IP reeks te starten (bv ^172.0.) of gebruik $ om een IP reeks te eindigen (bv .0.1$).';
+	$_lang['clientexceptions.label_ipnumber']							= 'IP adres';
+	$_lang['clientexceptions.label_ipnumber_desc']						= 'Gebruik ^ om een IP reeks te starten (vb. ^127.0.), gebruik $ om een IP reeks te eindigen (vb. .0.1$) en gebruik % om een wildcard te activeren.';
 	$_lang['clientexceptions.label_own_ipnumber']						= 'Mijn IP';
-	$_lang['clientexceptions.label_name']								= 'Naam';
-	$_lang['clientexceptions.label_name_desc']							= 'De naam van de uitzondering.';
+	$_lang['clientexceptions.label_name']								= 'Naam uitzondering';
+	$_lang['clientexceptions.label_name_desc']							= 'Voer naam van de uitzondering in.';
 	$_lang['clientexceptions.label_context']							= 'Context';
-	$_lang['clientexceptions.label_context_desc']						= 'De context van de uitzondering. Als er geen context geselecteerd word geldt deze uitzondering voor alle contexten.';
-	$_lang['clientexceptions.label_type']								= 'Uitzonderingstype';
-	$_lang['clientexceptions.label_type_desc']							= 'De type van de uitzondering, dit kan "weigeren" of "verlenen" zijn.';
-	$_lang['clientexceptions.label_description']						= 'Omschrijving/reden';
-	$_lang['clientexceptions.label_description_desc']					= 'De omschrijving/reden van de uitzondering.';
+	$_lang['clientexceptions.label_context_desc']						= 'Selecteer de context waarbinnen de uitzondering actief moet zijn. Als er geen context wordt geselecteerd, zal de uitzondering actief zijn in alle contexten!';
+	$_lang['clientexceptions.label_type']								= 'Uitzondering type';
+	$_lang['clientexceptions.label_type_desc']							= 'Weiger of verleen de toegang aan deze uitzondering.';
+	$_lang['clientexceptions.label_description']						= 'Omschrijving (optioneel)';
+	$_lang['clientexceptions.label_description_desc']					= 'Voeg een omschrijving toe.';
 	$_lang['clientexceptions.label_active']								= 'Actief';
 	$_lang['clientexceptions.label_active_desc']						= '';
 	$_lang['clientexceptions.label_import_file']						= 'Bestand';
 	$_lang['clientexceptions.label_import_file_desc']					= 'Selecteer een geldig CSV bestand.';
 	$_lang['clientexceptions.label_import_delimiter']					= 'Scheidingsteken';
-	$_lang['clientexceptions.label_import_delimiter_desc']				= 'Het scheidingsteken waarmee kolommen gescheiden worden. Standaard is ";".';
-	$_lang['clientexceptions.label_import_headers']						= 'Eerste rij zijn kolommen.';
+	$_lang['clientexceptions.label_import_delimiter_desc']				= 'Het scheidingsteken om kolommen te scheiden. Standaard is ";".';
+	$_lang['clientexceptions.label_import_headers']						= 'Eerste rij bestaat uit kolommen.';
 	$_lang['clientexceptions.label_import_headers_desc']				= '';
 	$_lang['clientexceptions.label_import_reset']						= 'Verwijder alle huidige uitzonderingen.';
 	$_lang['clientexceptions.label_import_reset_desc']					= '';
@@ -60,15 +60,14 @@
 	$_lang['clientexceptions.filter_context']							= 'Filter op context...';
 	$_lang['clientexceptions.filter_type']								= 'Filter op type...';
 	$_lang['clientexceptions.context_independent']						= 'Onafhankelijk';			
-	$_lang['clientexceptions.type_deny']								= 'Weigeren';
-	$_lang['clientexceptions.type_grant']								= 'Verlenen';
-	$_lang['clientexceptions.import_dir_failed']						= 'Er is een fout opgetreden tijdens het importeren van de uitzonderingen, de import folder kon niet aangemaakt worden.';
-	$_lang['clientexceptions.import_valid_failed']						= 'Selecteer een geldig CSV bestand.';
-	$_lang['clientexceptions.import_upload_failed']						= 'Er is een fout opgetreden tijdens het importeren van de uitzonderingen, het CSV bestand kon niet geüpload worden.';
-	$_lang['clientexceptions.import_read_failed']						= 'Er is een fout opgetreden tijdens het importeren van de uitzonderingen, het CSV bestand kon niet gelezen worden.';
-	$_lang['clientexceptions.export_failed']							= 'Het exporteren van de uitzonderingen is mislukt, probeer het nog eens.';
-	$_lang['clientexceptions.export_dir_failed']						= 'Er is een fout opgetreden tijdens het exporteren van de uitzonderingen, de export folder kon niet aangemaakt worden.';
+	$_lang['clientexceptions.type_deny']								= 'Toegang weigeren';
+	$_lang['clientexceptions.type_grant']								= 'Toegang verlenen';
+	$_lang['clientexceptions.import_dir_failed']						= 'Er is een fout opgetreden tijdens het importeren van de uitzonderingen, de import map kon niet worden aangemaakt.';
+	$_lang['clientexceptions.import_valid_failed']						= 'Selecteer een geldig CSV bestand!';
+	$_lang['clientexceptions.import_upload_failed']						= 'Er is een fout opgetreden tijdens het importeren van de uitzonderingen, het CSV bestand kon niet worden geüpload.';
+	$_lang['clientexceptions.import_read_failed']						= 'Er is een fout opgetreden tijdens het importeren van de uitzonderingen, het CSV bestand kon niet worden gelezen.';
+	$_lang['clientexceptions.export_failed']							= 'Het exporteren van de uitzonderingen is mislukt. Probeer het a.u.b. opnieuw.';
+	$_lang['clientexceptions.export_dir_failed']						= 'Er is een fout opgetreden tijdens het exporteren van de uitzonderingen, de export map kon niet worden aangemaakt.';
 	
-	$_lang['clientexceptions.error_own_ip']								= 'Het is niet mogelijk om jezelf de toegang te weigeren.';
-	
+	$_lang['clientexceptions.error_own_ip']								= 'Het is niet mogelijk om je eigen toegang te weigeren!';
 ?>
